ac12ba55b4ceaad1ec6abe11fd862c625f65ffe3
Brad <white.brad17@gmail.com>
2018-04-29 10:43:55 -0600

    Added voting form.

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+# Namespace for polls URLs
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponse, Http404
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from .models import Question
+from django.urls import reverse
+from .models import Choice, Question
 
 
 def index(request):
@@ -15,8 +17,22 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse("You're looking at the results of question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form
+        return render(request, 'polls/detail.html',
+                      {'question': question,
+                       'error_message': "You didn't select a choice",
+                       })
+    else:
+        # F() is used to avoid race conditions
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
